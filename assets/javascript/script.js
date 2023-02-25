8dfc06fa9d3678392f0bd12d999d770850d021a4
CorbinSpence <corbinspence16@gmail.com>
2023-02-25 14:16:19 -0500

    Added rough date display and schedule updater

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,10 @@
   The use of jquery, jquery UI, and dayjs is in current module and is allowed.
 */
 
+const pastClass = "row time-block past"
+const presentClass = "row time-block present"
+const futureClass = "row time-block future"
+var today = dayjs()
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -28,7 +32,7 @@ $(function () {
     let parent = button.parent('.time-block')
     //console.log(parent)
     let textPrompt = parent.children('textarea')
-    console.log(textPrompt)
+    //console.log(textPrompt)
     let text = textPrompt.val()
     localStorage.setItem(parent.attr('id'), text)
     //console.log(localStorage.getItem(parent.attr('id')))
@@ -40,7 +44,26 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  
+  //$('#hour-9').attr('class', futureClass)
+  let blocks = $('.container-fluid').children()
+  //console.log(blocks)
+  let hour = parseInt(today.format('H'))
+  console.log(hour)
+  //console.log(typeof hour)
+  for(let i=0; i<blocks.length; i++){
+    console.log(blocks[i])
+    //$(blocks[i]).attr('class', pastClass)
+    let hourValue = parseInt($(blocks[i]).attr('id').match(/[0-9]+/)[0])
+    console.log(hourValue)
+    if(hourValue<hour){
+      $(blocks[i]).attr('class', pastClass)
+    }else if(hourValue>hour){
+      $(blocks[i]).attr('class', futureClass)
+    }else{
+      $(blocks[i]).attr('class', presentClass)
+    }
+  }
+
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -49,7 +72,8 @@ $(function () {
 
 
   // TODO: Add code to display the current date in the header of the page.
-
-
+  let dateDisplay = $('#currentDay')
+  dateDisplay.text(today.format('dddd[,] MMMM D[th]'))
+  
 
 });
